eb8f627ed2f85581c62b636bf0ec3af299db1122
SHAKHAWAT HOSSIN <virtual.society.2023@.com>
2025-10-02 21:36:54 +0800

    first commit

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>In the Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
